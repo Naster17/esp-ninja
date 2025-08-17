9242cdf98e84fe74bacc3a2530630e1f00aa251f
Naster17 <nekjk17042005@gmail.com>
2025-08-17 17:13:20 +0200

    ui: touch + ui comlition

--- src/kernel/lib/ui/ui.cc
+++ src/kernel/lib/ui/ui.cc
@@ -1,4 +1,5 @@
 #include <drivers/screen.h>
+#include <drivers/touch.h>
 #include <lib/ui.h>
 
 ui_struct ui_st;
@@ -43,8 +44,7 @@ void ui_drawer(int32_t x, int32_t y, int32_t w, int32_t h, widget_t *widget)
         }
     }
 }
-
-void ui_run(grid_t *grid)
+void ui_draw(grid_t *grid)
 {
     if (grid->wt_head == NULL)
         return; // maybe fix this shity do while
@@ -52,24 +52,58 @@ void ui_run(grid_t *grid)
     widget_t *widget = grid->wt_head;
     do
     {
-        int32_t x, y;
-        int32_t w, h;
+        int32_t y = (grid->cell_height * widget->row) + ui_st.statbar_height;
+        int32_t h = grid->cell_height * widget->height;
 
-        y = (grid->cell_height * widget->row) + ui_st.statbar_height;
-        h = grid->cell_height;
+        int32_t x = grid->cell_width * widget->col;
+        int32_t w = grid->cell_width * widget->width;
+
         if (widget->row == 0)
-        {
             y = ui_st.statbar_height;
-            h = grid->cell_height;
-        }
-        x = grid->cell_width * widget->col;
-        w = grid->cell_width;
         if (widget->col == 0)
-        {
             x = 0;
-            w = grid->cell_width;
-        }
-        ui_drawer(x, y, w * widget->width, h * widget->height, widget);
+
+        ui_drawer(x, y, w, h, widget);
 
     } while ((widget = widget->next) != NULL);
+}
+#include <drivers/serial.h>
+
+void ui_touch_handler(grid_t *grid, touch_state_t *st)
+{
+    widget_t *widget = grid->wt_head;
+    do
+    {
+        int32_t y = (grid->cell_height * widget->row) + ui_st.statbar_height;
+        int32_t h = grid->cell_height * widget->height;
+
+        int32_t x = grid->cell_width * widget->col;
+        int32_t w = grid->cell_width * widget->width;
+
+        if (widget->row == 0)
+            y = ui_st.statbar_height;
+        if (widget->col == 0)
+            x = 0;
+
+        if (st->point.x < (x + w) && st->point.x > x && st->point.y < (y + h) && st->point.y > y)
+        {
+            serial_print(widget->label);
+        }
+
+    } while ((widget = widget->next) != NULL);
+}
+
+void ui_run(grid_t *grid)
+{
+    ui_draw(grid);
+    for (;;)
+    {
+        touch_state_t st = touch_get_state();
+        if (st.state != TOUCH_NONE)
+        {
+            ui_touch_handler(grid, &st);
+            ui_draw(grid);
+            delay(100);
+        }
+    }
 }
